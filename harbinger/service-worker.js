d496a36054310d8c5a40c2bc2e620e84c4140542
Abdul-Harisu Inusah <abdul@blockwatch.cc>
2022-02-09 09:18:53 +0000

    update harbinger

--- harbinger/service-worker.js
+++ harbinger/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/polywrap-tezos-demo/precache-manifest.3b8dea1a57d9e5bd29abf9cfc89b226d.js"
+  "/precache-manifest.bef52308d8e5109658edea0a276b46aa.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/polywrap-tezos-demo/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
