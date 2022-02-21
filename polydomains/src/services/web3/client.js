9695edbbae75cb00313e2c1d27528c0f196630fd
Abdul-Harisu Inusah <abdul@blockwatch.cc>
2022-02-21 14:06:53 +0000

    chore(): update polywrap lib

--- polydomains/src/services/web3/client.js
+++ polydomains/src/services/web3/client.js
@@ -3,7 +3,7 @@ import { tezosDomainsPlugin } from "@web3api/tezos-domains-plugin-js"
 import { tezosPlugin } from "@web3api/tezos-plugin-js"
 
 export const TEZOS_PLUGIN_JS = 'w3://ens/tezos.web3api.eth'
-export const TEZOS_DOMAINS_WRAPPER_URI = 'w3://ipfs/QmSbeyrDnWXMPrJbNyCVWbHFevwP2BWvgFMGEZNby3KKeQ'
+export const TEZOS_DOMAINS_WRAPPER_URI = 'w3://ipfs/QmTeyTLBZqY5VZtMgfAbHS6ePF8PYsvaLThCM8Ke64qc6h'
 export const TEZOS_DOMAINS_PLUGIN_JS = 'w3://ens/tezosDomainsPlugin.web3api.eth'
 
 
