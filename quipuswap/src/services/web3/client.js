b34c9411dcd5d787c8cc66b7ced7e7b03713c546
von-dee <vondeesela@gmail.com>
2022-03-14 09:21:29 +0000

    Updated Client Js

--- quipuswap/src/services/web3/client.js
+++ quipuswap/src/services/web3/client.js
@@ -2,7 +2,7 @@ import { Web3ApiClient } from "@web3api/client-js"
 import { tezosPlugin } from "@web3api/tezos-plugin-js"
 
 
-export const TEZOS_QUIPUSWAP_WRAPPER_URI = 'w3://ipfs/QmThx6GPYQof5TrwjProELc9qktKNYXADW2sjqJxRB6bFE'
+export const TEZOS_QUIPUSWAP_WRAPPER_URI = 'w3://ipfs/QmXvpdtxpDSUEVCvApC2hYrsnVEJPYxrmBn4HkTfWtMW1A'
 export const TEZOS_PLUGIN_JS = 'w3://ens/tezos.web3api.eth'
 
 export const TezosConnections = {
