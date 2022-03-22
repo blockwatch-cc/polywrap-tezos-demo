181d34ebff2acedc0f70ae4e83745961f0ed7f42
von-dee <vondeesela@gmail.com>
2022-03-22 09:06:54 +0000

    Updated the Wrapper Hash

--- quipuswap/src/services/web3/client.js
+++ quipuswap/src/services/web3/client.js
@@ -2,7 +2,8 @@ import { Web3ApiClient } from "@web3api/client-js"
 import { tezosPlugin } from "@web3api/tezos-plugin-js"
 
 
-export const TEZOS_QUIPUSWAP_WRAPPER_URI = 'w3://ipfs/QmSySUvrC6At5GcZN8yJwPAKAcfzhLq5T33Qu6rWLKw3eG'
+// export const TEZOS_QUIPUSWAP_WRAPPER_URI = 'w3://ipfs/QmSySUvrC6At5GcZN8yJwPAKAcfzhLq5T33Qu6rWLKw3eG'
+export const TEZOS_QUIPUSWAP_WRAPPER_URI = 'w3://ipfs/QmYSJ3dHJuNWygFtBThKHNB7Na1mSq7uSCLYE4991CQkZY'
 export const TEZOS_PLUGIN_JS = 'w3://ens/tezos.web3api.eth'
 
 export const TezosConnections = {
