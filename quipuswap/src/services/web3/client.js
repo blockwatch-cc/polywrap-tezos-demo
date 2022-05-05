8281d69b39d9a17c46f8ecb601494cfba8c43578
Abdul-Harisu Inusah <abdul@blockwatch.cc>
2022-05-05 13:12:54 +0000

    chore(): remove ipfs plugin support since pinata rate limits their api

--- quipuswap/src/services/web3/client.js
+++ quipuswap/src/services/web3/client.js
@@ -1,7 +1,5 @@
 import { Web3ApiClient } from "@web3api/client-js"
 import { tezosPlugin } from "@blockwatch-cc/tezos-plugin-js"
-import { ipfsPlugin } from "@web3api/ipfs-plugin-js"
-
 
 export const TEZOS_QUIPUSWAP_WRAPPER_URI = 'w3://ipfs/QmQsnEa1dB92uWDYB4grpC9YC4x9BsHscmWXPKUU4Uor8z'
 export const TEZOS_PLUGIN_JS = 'w3://ens/tezos.web3api.eth'
@@ -37,12 +35,6 @@ export const client = new Web3ApiClient({
             }
         },
         defaultNetwork: "ithacanet"
-      })
-    },
-    {
-      uri: 'w3://ens/ipfs.web3api.eth',
-      plugin: ipfsPlugin({
-        provider: 'https://gateway.pinata.cloud'
       })
     }
   ]
